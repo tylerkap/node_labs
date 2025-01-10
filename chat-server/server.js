c14044c4893e89a8a6986ba5592f901324eb7622
Tyler Kap <tylerkap55@gmail.com>
2025-01-10 16:20:17 -0700

    minor changes to server.js

--- chat-server/server.js
+++ chat-server/server.js
@@ -36,17 +36,3 @@ function broadcast(message, socketSent) {
         });
     }
 }
-
-
-
-
-
-
-// process.stdin.on('readable', () => { 
-//     let chunk; 
-//     // Use a loop to make sure we read all available data. 
-//     while ((chunk = process.stdin.read()) !== null) { 
-//      process.stdout.write(`data: ${chunk}`);
-
-//     }
-// });
